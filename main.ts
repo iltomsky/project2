05c26630f7d9bb2d32c99e81b08723069009617f
iltomsky <nathan.arnouts2012@gmail.com>
2024-11-25 12:20:20 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,10 @@
+loops.everyInterval(100, function () {
+    led.stopAnimation()
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        `)
+})
